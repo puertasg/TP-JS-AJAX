3d70b784df5595b1d0292459708dd2a327b38bcf
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-05-15 15:50:54 +0200

    Ajout fonction addTask, suppression console.log

--- js/tasklist.js
+++ js/tasklist.js
@@ -2,24 +2,28 @@ function removeTask(task)
 {
 	if(window.confirm("Souhaitez vous supprimer la tache " + task.innerHTML + " ?"))
 	{
-		console.log("remove task");
 		task.remove();
 	}
+}
+
+function addTask()
+{
+	var task = document.getElementById("todotxt").value;
+		
+	var newTask = document.createElement("li");
+	var newTaskText = document.createTextNode(task);
+	newTask.appendChild(newTaskText);
+	newTask.addEventListener("click", function() {
+		removeTask(this);
+	});
+	
+	document.getElementById("todolist").appendChild(newTask);
 }
 
 function loading()
 {
 	button = document.getElementById("addtodo");
 	button.addEventListener('click', function(event) {
-		var task = document.getElementById("todotxt").value;
-		
-		var newTask = document.createElement("li");
-		var newTaskText = document.createTextNode(task);
-		newTask.appendChild(newTaskText);
-		newTask.addEventListener("click", function() {
-			removeTask(this);
-		});
-		
-		document.getElementById("todolist").appendChild(newTask);
+		addTask();
 	});
 }
